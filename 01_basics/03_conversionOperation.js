50fe7cfeb04d62538c4e4a426077b702f518189f
Harshit Gupta <149142309+harshitgupta240197@users.noreply.github.com>
2025-05-11 05:23:04 +0000

    Conversion of Data Types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,43 @@
+let score = "33"
+
+console.log(typeof score)
+console.log(typeof (score))  //Using METHOD by putting inside parenthesis
+
+let valueInNumber = Number(score) //Converting the string to Number
+
+console.log(typeof score)
+console.log(typeof valueInNumber)
+
+
+
+let marks = "44abs" //This is a string
+let marksInNumber = Number(marks) //Coversion will convert string to a number
+console.log(typeof marks)
+console.log(typeof marksInNumber)
+console.log(marksInNumber) //Printing this will produce NaN (Not a Number)
+
+
+let subject = null //This is treated as an object in JS
+let subjectInNumber = Number(subject) //Coversion will convert null to a number
+console.log(typeof subject)
+console.log(typeof subjectInNumber)
+console.log(subjectInNumber) //Printing this will produce 0
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false = 0
+
+let isLoggedIn = 1 //If we leave this value as empty then the result will be False else True
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(typeof isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+
+// 1 => True
+// => False
+// "String" => True
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
