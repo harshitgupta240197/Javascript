96ffd31c1874a3c6145357f13810350fecf32a05
Harshit Gupta <149142309+harshitgupta240197@users.noreply.github.com>
2025-05-12 11:41:09 +0000

    Conversion Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -41,3 +41,48 @@ console.log(typeof booleanIsLoggedIn)
 let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber)
+
+
+
+
+// ************************************** OPERATIONS **************************************//
+
+let value = 3
+let negativeValue = -value
+console.log(negativeValue) //The result will be -3
+
+console.log(2+3) //Result will be 5
+
+let str1 = "Hello"
+let str2 = " Harshit"
+console.log(str1 + str2)
+
+//Or we can do it another way like this
+let str3 = str1 + str2
+console.log(str3)
+
+
+console.log("1" + 4)
+console.log("Harshit" + 4)
+
+console.log(true) //This will return True
+console.log(+true) //This will return 1
+console.log(+"") //This will return 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+// Postfix operator
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter) // THis will give 101
+
+//******or*******//
+
+// Prefix operator
+let gameCounterr = 100
+++gameCounterr;
+console.log(gameCounterr) // This will give 101 as well
+
+
+// Link to the article : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
