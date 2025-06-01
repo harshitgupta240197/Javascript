33663312defbb7681851cdc664c3bdc3cada6026
Harshit Gupta <biogenius6@gmail.com>
2025-06-01 18:33:08 +0530

    Scopes, Functions, If conditions

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -29,3 +29,69 @@ if (true){
 }
 
 
+
+
+
+//***************************************************************** NESTED SCOPE *******************************************************/
+function one(){
+    const username = "Harshit"
+    //Nesting another function inside the present function
+    function two(){
+        const owner = "microsoft"
+        console.log(username);
+        
+    }
+    // console.log(owner); // Will result in not defined
+
+    two()
+    
+}
+
+one()
+
+
+
+
+
+
+
+// Nested IF conditions
+if (true) {
+    const bigName = "Harshit"
+    if (bigName === "Harshit") {
+        const website = " youtube"
+        console.log(bigName + website);
+    }
+    // console.log(website); // This will cause an error 
+    
+}
+// console.log(bigName); // This will cause an error
+
+
+
+
+
+
+
+
+//*********************************** Functions are made using two ways **************************** */
+
+// We will make a function that adds 1 to any given number
+// In this case we can access the function beofre it's declaration
+function addOne(num){
+    return num + 1
+}
+addOne(5) // Calling the function
+console.log(addOne(5)); // This will result as 6
+// If I place this console log before the function then too it will produce the result
+
+
+// Sicne this function is being held inside a variable so we cannot access the function only after it is declared
+// Another function that will add 2
+// This is also a function but sometimes referred to as EXPRESSION
+const addTWo = function(num){
+    return num + 2
+}
+addTWo(7)
+console.log(addTWo(7)); // This will return 9
+// If I palce this console log before the expression then it will throw an error
