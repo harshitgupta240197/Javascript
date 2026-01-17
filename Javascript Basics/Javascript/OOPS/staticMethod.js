b866c5f1c77b6a7d62974b1c9ee2e6b7792be47d
Harshit Gupta <biogenius6@gmail.com>
2026-01-17 06:29:42 +0530

    // Polymorphism and Abstraction

---
+++ Javascript Basics/Javascript/OOPS/staticMethod.js
@@ -0,0 +1,4 @@
+// STATIC METHOD
+/**A static method in JavaScript is a function that belongs to the class itself, rather than any specific 
+ instance (object) of the class. It is defined using the static keyword and is called directly on the class name.  */
+ 
