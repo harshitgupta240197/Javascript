294d1d2662a6744d9a6fbf6802b920ee0049cd75
Harshit Gupta <biogenius6@gmail.com>
2026-08-15 16:00:02 +0530

    SWITCH statements

--- Practice_JS/app.js
+++ Practice_JS/app.js
@@ -1,5 +1,18 @@
-if (2 > 1) {
-    console.log(true)
-} else {
-    console.log(false);
+const day = 2;
+
+switch (day) {
+    case 1:
+        console.log('Monday');
+        break;
+    case 2:
+        console.log('Tuesday');
+        break;
+    case 3:
+        console.log('Wednesday');
+        break;
+    case 4:
+        console.log('Thursday');
+        break;
+    default:
+        console.log('I dont know that');
 }
