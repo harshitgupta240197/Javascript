c2a681d9df3e14688ab5601c4e97839b4b2916c6
Harshit Gupta <biogenius6@gmail.com>
2026-08-16 17:24:42 +0530

    Built a small terminal based todo app using JS

--- Practice_JS/app.js
+++ Practice_JS/app.js
@@ -1,18 +1,30 @@
-const day = 2;
+let question = prompt('Welcome to ToDo App! What do you want to do today ?')
+const storage = []
 
-switch (day) {
-    case 1:
-        console.log('Monday');
-        break;
-    case 2:
-        console.log('Tuesday');
-        break;
-    case 3:
-        console.log('Wednesday');
-        break;
-    case 4:
-        console.log('Thursday');
-        break;
-    default:
-        console.log('I dont know that');
-}
+while (question !== 'q') {
+    if (question === 'new') {
+        let newToDo = prompt('Enter New ToDo: ');
+        storage.push(newToDo);
+        console.log(`Added ${newToDo} to the list`);
+    } else if (question === 'list') {
+        for (const word of storage) {
+            console.log(word);
+        }
+    } else if (question === 'delete') {
+        let deleteWhat = prompt('What do you want to delete ? ')
+        const index = storage.indexOf(deleteWhat)
+        if (index !== -1) {
+            storage.splice(index, 1)
+            console.log(`Deleted ${deleteWhat}`);
+        } else {
+            console.log(`Not found ${deleteWhat}`);
+            
+        }
+    } 
+    question = prompt('Welcome to ToDo App! What do you want to do today ?')
+    } 
+    
+console.log('Ok quitting now');
+
+
+
