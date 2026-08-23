e959558515ba2d67ebf8cbd47049ddc57ecdc62d
Harshit Gupta <biogenius6@gmail.com>
2026-08-23 14:38:42 +0530

    Path Parameters in JS

--- Express JS/Express App/index.js
+++ Express JS/Express App/index.js
@@ -4,11 +4,41 @@ const app = express()
 // console.dir(app)
 
 // Parse incoming requests
-app.use((req, res) => {
-    console.log('We got a new request')
-    // res.send('Hey! We got your response')
-    // res.send({color: 'red'})
-    res.send('<h1>This is my webpage</h1>')
+// app.use((req, res) => {
+//     console.log('We got a new request')
+//     // res.send('Hey! We got your response')
+//     // res.send({color: 'red'})
+//     res.send('<h1>This is my webpage</h1>')
+// })
+
+// GET REQUESTS
+
+app.get('/', (req, res) => {
+    res.send('This is the home page')
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const {subreddit} = req.params;
+    res.send(`<h1>Browsing the ${subreddit} subreddit</h1>`)
+})
+
+app.get('/dogs', (req, res) => {
+    res.send('Hey there')
+})
+
+app.get('/cows', (req, res) => {
+    res.send('MAWWWW')
+})
+
+// POST REQUEST
+
+app.post('/dogs', (req, res) => {
+    res.send('This is a post request')
+})
+
+// GENERIC REQUEST
+app.get('/{*path}', (req, res, next) => {
+    res.send('Oops We Dont Know This Path')
 })
 
 // We will listen on localhost 3000
