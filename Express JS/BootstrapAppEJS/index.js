35248ca5379097f0d9a5a1210106185d4ea76402
Harshit Gupta <biogenius6@gmail.com>
2026-08-24 00:01:23 +0530

    Bootstrap + Express

--- Express JS/BootstrapAppEJS/index.js
+++ Express JS/BootstrapAppEJS/index.js
@@ -5,7 +5,7 @@ const redditData = require('./data.json')
 console.log(redditData);
 
 // Serving static assets in Express
-pp.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 // Setting EJS as the View Engine
