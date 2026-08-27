04a1216f1e40833bfe1aa51bd2849c4c954f72f9
Harshit Gupta <biogenius6@gmail.com>
2026-08-27 12:46:27 +0530

    Added JS to add new campgrounds in the project app

--- Project/YelpCamp/app.js
+++ Project/YelpCamp/app.js
@@ -19,16 +19,36 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+// Setting up the body parser
+app.use(express.urlencoded({ extended: true }))
+
 // Home page
 app.get('/', (req, res) => {
     res.render('home')
 })
 
+// Campgrounds Page
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({})
+    res.render('campgrounds/index', { campgrounds })
+})
+
 // Making New Campground
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({})
-    await camp.save()
-    res.send(camp)
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+// End point for submitting form for new campground
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+// Campgrounds by ID
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', { campground })
 })
 
 // Port set up
