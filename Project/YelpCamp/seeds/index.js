8cc10e3875dcb87d4456920b55701b76fc9c214d
Harshit Gupta <biogenius6@gmail.com>
2026-08-27 09:32:58 +0530

    Prepared and deployed a seed file into our application

--- Project/YelpCamp/seeds/index.js
+++ Project/YelpCamp/seeds/index.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
-const Campground = require('./models/campground')
+const Campground = require('../models/campground')
+const cities = require('./cities')
+const { places, descriptors} = require('./seedHelpers')
 
 // Connection details:
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
@@ -11,3 +13,25 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
         console.log('Error in connecting to the database!');
         console.log(err);
     })
+
+// Creating a RANDOM function
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+
+// Im the seed DB first we will delete everything
+const seedDB = async () => {
+    await Campground.deleteMany({})
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save();
+    }
+}
+
+// Since this a seedDB so we will close the connection
+seedDB().then(() => {
+    mongoose.connection.close()
+})
