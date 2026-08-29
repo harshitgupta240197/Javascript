e3ea8c2e53d8511762f17f766a4a304b9ff9791c
Harshit Gupta <biogenius6@gmail.com>
2026-08-29 14:37:54 +0530

    Added feature of giving reviews

--- Project/YelpCamp/app.js
+++ Project/YelpCamp/app.js
@@ -9,7 +9,8 @@ const catchAsync = require('./utils/catchAsync')
 const ExpressError = require('./utils/ExpressError')
 const Joi = require('joi')
 const { title } = require('process')
-const { campgroundSchema } = require('./schemas')
+const { campgroundSchema, reviewSchema } = require('./schemas')
+const Review = require('./models/review')
 
 // Connection details:
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
@@ -33,6 +34,16 @@ app.use(methodOverride('_method'));
 
 const validateCampground = (req, res, next) => {
     const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
     if (error) {
         const msg = error.details.map(el => el.message).join(',')
         throw new ExpressError(msg, 400)
@@ -66,7 +77,7 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) =
 
 // Campgrounds by ID
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
+    const campground = await Campground.findById(req.params.id).populate('reviews');
     res.render('campgrounds/show', { campground })
 }))
 
@@ -88,6 +99,15 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}))
+
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 // Error handling if nothing runs above this - CATCH ALL ROUTE
