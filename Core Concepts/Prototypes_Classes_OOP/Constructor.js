07ee81cbd0f8b2ee82d1334abe1fed2a502e301b
Harshit Gupta <biogenius6@gmail.com>
2026-08-31 15:21:48 +0530

    Functional Constructors and New keyword in JS OOP

---
+++ Core Concepts/Prototypes_Classes_OOP/Constructor.js
@@ -0,0 +1,15 @@
+function Person(name, age) {
+    this.name = name
+    this.age = age
+}
+
+function Car(make, model) {
+    this.make = make
+    this.model = model
+}
+
+function User(name, age, username) {
+    this.name = name
+    this.age = age
+    this.username = username
+}
