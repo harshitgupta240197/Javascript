2db04cf276b2603dd6c1c6454a7bcde146da733f
Harshit Gupta <biogenius6@gmail.com>
2026-09-06 23:58:32 +0530

    Creating a basic counter using useState hook in reactJS

---
+++ ReactJS/Projects/react-state/src/Counter.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function Counter() {
+
+    const [num, setNum] = useState(0)
+
+    const changeNum = () => {
+        setNum(num + 1)
+    }
+
+    return (
+        <div>
+            <p>The count is: {num} </p>
+            <button onClick={changeNum}>
+                Increment
+            </button>
+        </div>
+    )
+    
+}
+
+export default Counter;
