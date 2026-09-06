14255f8ac48efb37d59484fb85a3f3ec729aab20
Harshit Gupta <biogenius6@gmail.com>
2026-09-06 16:40:04 +0530

    Added conditional rendering to ShoppingList mini app in reactJS

--- ReactJS/Projects/shopping-cart/src/ShoppingList.jsx
+++ ReactJS/Projects/shopping-cart/src/ShoppingList.jsx
@@ -1,15 +1,17 @@
+import ShoppingListItem from "./ShoppingListItem";
+
 function ShoppingList({ items }) {
 
     return (
         <div>
             <ul>
                 {items.map((i) => (
-                    <li key={i.id}
-                        style={{color: i.completed ? 'grey' : 'black',
-                        textDecoration: i.completed ? 'line-through' : 'none'
-                    }}>
-                        {i.item} - {i.quantity}
-                    </li>
+                    <ShoppingListItem
+                        key={i.id}
+                        item={i.item}
+                        quantity={i.quantity}
+                        completed={i.completed}
+                    />
                 ))}
             </ul>
         </div>
