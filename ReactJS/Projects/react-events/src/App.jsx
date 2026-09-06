5f08488ead6ea169c0ab455eb91527fef4eae054
Harshit Gupta <biogenius6@gmail.com>
2026-09-06 19:46:10 +0530

    mini react event clicker application

---
+++ ReactJS/Projects/react-events/src/App.jsx
@@ -0,0 +1,12 @@
+import Clicker from './Clicker'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Clicker message='Hii!' buttonText='Please Click Me' />
+    </div>
+  )
+}
+
+export default App
---
+++ ReactJS/Projects/react-events/src/Clicker.jsx
@@ -0,0 +1,5 @@
+function Clicker({message, buttonText}) {
+    return <button onClick={() => alert(message)}>{buttonText}</button>
+}
+
+export default Clicker;
